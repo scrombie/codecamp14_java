7305a6651cad2fcd56db0e24de1b0f49eae7cea8
Matthew Mayaki <dynamicmax5000@gmail.com>
2014-08-15 14:31:22 +0100

    updated contactGroups.java
    Added canCall and doNotTakeCall methods

--- com.codeCamp14.lab/src/com/codecamp14/lab/ContactsGroup.java
+++ com.codeCamp14.lab/src/com/codecamp14/lab/ContactsGroup.java
@@ -18,13 +18,19 @@ public class ContactsGroup {
         this.groupContacts = contacts;
         this.groupName = name;
         this.takeCalls = takeCalls;
+        this.maxContacts = 100;
     }
 
     public ContactsGroup(String name, Person[] contacts, boolean takeCalls, int maxContacts) {
         this.groupContacts = contacts;
         this.groupName = name;
         this.takeCalls = takeCalls;
-        this.maxContacts= maxContacts;
+        if(maxContacts > 100){
+            System.out.println("Out of range...Max Contacts reset to 100");
+        }else{
+            this.maxContacts= maxContacts;
+        }
+        
     }
 
     public Person[] getGroupContacts() {
@@ -39,11 +45,50 @@ public class ContactsGroup {
         return maxContacts;
     }
 
-    public boolean isTakeCalls() {
+    public boolean canCall() {
         return takeCalls;
     }
     
+    public double getAverageAge(){
+        double age = 0;
+        for(int i=0;i<this.groupContacts.length;i++){
+            age+=(double)groupContacts[i].getAge();
+        }
+        age = age/groupContacts.length;
+        return age;
+    }
     
+    public void addContact(Person p){
+        if(this.groupContacts.length < this.maxContacts){
+            for(int i=0;i<this.groupContacts.length;i++){
+                if(groupContacts[i]== null){
+                    groupContacts[i] = p;
+                    System.out.println("Contact Added");
+                }else{
+                    System.out.println("Contact Group Full");
+                }
+            }
+        }
+    }
+
+    public void setTakeCalls(boolean takeCalls) {
+        this.takeCalls = takeCalls;
+    }
+    
+    public boolean contactInGroup(Person p){
+        for(int i=0;i<this.groupContacts.length;i++){
+            if(groupContacts[i].getName().equals(p.getName())){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void doNotTakeCalls(ContactsGroup[] contactGroup){
+        for(int i=0;i<contactGroup.length;i++){
+            contactGroup[i].setTakeCalls(false);
+        }
+    }
 
     /**
      * @param args the command line arguments
